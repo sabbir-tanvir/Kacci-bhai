17590ecdd33fe891d94a25e8486461acdd3a0041
Sabbir <sabbirmahmudtanvir@gmail.com>
2024-04-09 12:04:26 +0600

    Initial commit

---
+++ style.js
@@ -0,0 +1,8 @@
+
+
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function(){
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
